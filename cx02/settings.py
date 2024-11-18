7b9920b24016af0c5717c03e27dab0179734fafc
xx-fufu <1848198651@qq.com>
2024-11-18 19:00:20 +0800

    Update settings.py

--- cx02/settings.py
+++ cx02/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = "cx02.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'minidouban',
+        'USER': 'minidouban',
+        'PASSWORD': 'fPmXsrBiBf2E6fxi',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
